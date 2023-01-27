7e4d76e5e55346866f530ec3b2c8dd7f507a81a8
Mikhail Lyalin <mc.lyalin@gmail.com>
2023-01-27 20:14:21 +0500

    In process..

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,5 +1,14 @@
-from selenium import webdriver
-from selenium.webdriver.chrome.service import Service as ChromeService
-from webdriver_manager.chrome import ChromeDriverManager
+# import pytest
 
-driver = webdriver.Chrome(service=ChromeService(ChromeDriverManager().install()))
+# from selenium import webdriver
+# from selenium.webdriver.chrome.service import Service as ChromeService
+# from webdriver_manager.chrome import ChromeDriverManager
+
+
+# @pytest.fixture
+# def driver():
+#     driver = webdriver.Chrome(service=ChromeService(ChromeDriverManager().install()))
+
+#     yield driver
+
+#     driver.close()
