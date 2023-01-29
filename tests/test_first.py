c13169e36eaac85e8674a0dd303e0f94ceb48f09
Mikhail Lyalin <mc.lyalin@gmail.com>
2023-01-29 20:46:12 +0500

    In process..

--- tests/test_first.py
+++ tests/test_first.py
@@ -3,9 +3,16 @@ from selenium.webdriver.chrome.service import Service as ChromeService
 from webdriver_manager.chrome import ChromeDriverManager
 
 from selenium.webdriver.common.by import By
+from selenium.webdriver.support.wait import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
 
 
 driver = webdriver.Chrome(service=ChromeService(ChromeDriverManager().install()))
 
 driver.get('https://google.com')
-input_search = driver.find_element(By.NAME, "q")
+input_search = driver.find_element(By.NAME, 'q')
+input_search.send_keys('first test')
+WebDriverWait(driver, 10).until(
+    EC.element_to_be_clickable((By.XPATH, '//div[@class="aajZCb"]/descendant::input[@name="btnK"]'))
+).click()
+driver.save_screenshot('result.png')
