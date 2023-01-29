ea18d4c9fa79d7ab5df7c4c61692b6462378e229
Mikhail Lyalin <mc.lyalin@gmail.com>
2023-01-29 19:32:50 +0500

    In process..

---
+++ tests/test_first.py
@@ -0,0 +1,11 @@
+from selenium import webdriver
+from selenium.webdriver.chrome.service import Service as ChromeService
+from webdriver_manager.chrome import ChromeDriverManager
+
+from selenium.webdriver.common.by import By
+
+
+driver = webdriver.Chrome(service=ChromeService(ChromeDriverManager().install()))
+
+driver.get('https://google.com')
+input_search = driver.find_element(By.NAME, "q")
